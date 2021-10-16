9e7f0fda8069af5ccc0b8dbd224509e4b02fcad0
rawdasherif <rawda.sherif@ibtikar.net.sa>
2021-10-17 01:03:13 +0200

    Install required packages for node

---
+++ server.js
@@ -0,0 +1,14 @@
+// Required packages for app
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+app.use(cors());
+
+
