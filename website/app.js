a3c1ad89319c3f663a44f0e6d8998e1a47acfccf
rawdasherif <rawda.sherif@ibtikar.net.sa>
2021-10-17 02:50:55 +0200

    Integrate with open weather map api to get data

--- website/app.js
+++ website/app.js
@@ -1,4 +1,4 @@
-  /** Listen to get Weather Button */
+/** Listen to get Weather Button */
    document.getElementById('getWeather-btn').addEventListener('click', handleData);
 
 /******* **********************************************************************/
@@ -6,6 +6,10 @@
 
 function handleData(event) {
     zipCode = checkAndGetZipCode();
+    if (zipCode){
+        countryCode = document.getElementById('countryCode').value;
+        retriveWeather(zipCode, countryCode ? countryCode : 'us');
+    }
 }
 
 /** Check existance of zip code */
@@ -16,4 +20,20 @@ function checkAndGetZipCode()
     zipCodeTextError.textContent = !zipCode ?
      'Please Enter Zip Code .': '';
     return zipCode;
+}
+
+/** Retrive weather data from open weather map  */
+const retriveWeather = async (zipCode, countryCode) => {
+    apikey = '5cec774feeecae7ce6a902b66a72c634';
+    await (fetch(`https:api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&&appid=${apikey}`))
+        .then(function (resp) { return resp.json() }) // Convert data to json
+        .then(function (data) {
+            if(data.cod != 200){
+                alert(data.message)
+            }
+            return data;
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
 }
